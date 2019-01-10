f0f10518410047be97ee6e9bbe7c712e199815c3
Tymonr <tymonr@yandex.ru>
2019-01-10 21:44:47 +0300

    css added

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,11 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
     ]
   },
   //Dev server {
